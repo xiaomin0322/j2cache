b8c66c690fdef768516db196f17011e25b9a3d6d
zhangzengmin <zhangzengmin@cydeng.com>
2017-07-06 16:04:11 +0800

    123123

--- j2cache/src/main/java/net/oschina/j2cache/util/ConfigUtils.java
+++ j2cache/src/main/java/net/oschina/j2cache/util/ConfigUtils.java
@@ -4,10 +4,14 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.oschina.j2cache.CacheException;
+import net.oschina.j2cache.ehcache.EhCacheProvider;
 
 public class ConfigUtils {
-
+	 private final static Logger log = LoggerFactory.getLogger(ConfigUtils.class);
 	public static URL getURL(String remoteConfigPath, String localConfigPath) {
 		URL xml = null;
 		if (remoteConfigPath != null && !"".equals(remoteConfigPath)) {
@@ -17,11 +21,13 @@ public class ConfigUtils {
 				} else {
 					xml = new URL("file", "", remoteConfigPath);
 				}
+				log.info("load remoteConfigPath >>>>>> {}",remoteConfigPath);
 			} catch (MalformedURLException e) {
 				throw new CacheException("find ehcache.xml exception !!! "
 						+ remoteConfigPath);
 			}
 		} else {
+			log.info("load localConfigPath >>>>>> {}",localConfigPath);
 			xml = Thread.currentThread().getContextClassLoader().getResource(localConfigPath);
 			if (xml == null)
 				throw new CacheException("cannot find ehcache.xml !!! "+localConfigPath);
