0e3c0cfa843f3cb0435704a955b8147d84ac564d
zhangzengmin <zhangzengmin@cydeng.com>
2017-07-11 18:19:36 +0800

    123123

--- j2cache/src/main/java/net/oschina/j2cache/util/CurrentLimitRedisUtils.java
+++ j2cache/src/main/java/net/oschina/j2cache/util/CurrentLimitRedisUtils.java
@@ -3,6 +3,9 @@ package net.oschina.j2cache.util;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
@@ -25,11 +28,28 @@ public class CurrentLimitRedisUtils {
 	private static final ConcurrentHashMap<String, Semaphore> SEMAPHORE_MAP = new ConcurrentHashMap<String, Semaphore>();
 	
 	
-	public static final String DEF_REDIS_VAL = "NULL";
+	
+   private static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
+	
+	//空缓存名称
+	public static final String CACHE_VAL = "NULL";
+	
+	//最大并发数
+	public static final int PERMITS = 10;
+
+	//获取锁等待时间 seconds， 0:一直等待 
+	public static final int PERMITS_ACQUIRE_TIMEOUT = 0;
+	
+	//liveTime 空缓存失效时间 seconds
+	public static final int CACHE_LIVE_TIME = 60;
+	
+	//Callable 方法执行超时时间 seconds
+	public static final int CALL_TIMEOUT = 5;
+	
 
 	
 	public static <T> T get(String lockName,String cacheKey,Callable<T> callable,int permits) {
-		return get(lockName,cacheKey,callable, permits, 0);
+		return get(lockName,cacheKey,callable, PERMITS, PERMITS_ACQUIRE_TIMEOUT,CACHE_LIVE_TIME,CALL_TIMEOUT);
 	}
 	/**
 	 * 获取缓存中的数据
@@ -41,7 +61,7 @@ public class CurrentLimitRedisUtils {
 	 * @return obj
 	 */
 	@SuppressWarnings("unchecked")
-	public static <T> T get(String lockName,String cacheKey, Callable<T> callable,int permits,int permitsAcquireTimeOut) {
+	public  static <T> T get(String lockName,String cacheKey, Callable<T> callable,int permits,int permitsAcquireTimeOut,long liveTime,long callTimeout){
 		Object object = null;
 		byte[] cacheKeyBytes  = null;
 		try {
@@ -57,7 +77,7 @@ public class CurrentLimitRedisUtils {
 		}
 		
 		if(object !=null){
-			if(DEF_REDIS_VAL.equals(object)){
+			if(CACHE_VAL.equals(object)){
 				return null;
 			}else{
 				return (T) object;
@@ -94,21 +114,23 @@ public class CurrentLimitRedisUtils {
 			bytes = RedisClusterCacheProvider.getResource().get(cacheKeyBytes);
 			object = SerializationUtils.deserialize(bytes);
 			if(object !=null){
-				if(DEF_REDIS_VAL.equals(object)){
+				if(CACHE_VAL.equals(object)){
 					return null;
 				}else{
 					return (T) object;
 				}
 			}
 			//查询数据库
-			object = callable.call();
+			Future<T> future = EXECUTOR_SERVICE.submit(callable);
+			//查询数据库
+			object = future.get(callTimeout, TimeUnit.SECONDS);
 			
-			object = object == null ? DEF_REDIS_VAL : object;
+			object = object == null ? CACHE_VAL : object;
 			
 			//加入缓存
 			RedisClusterCacheProvider.getResource().set(cacheKeyBytes, SerializationUtils.serialize(object));
 			//callable为null则，缓存空结果，防止缓存穿透
-			if(DEF_REDIS_VAL.equals(object)){
+			if(CACHE_VAL.equals(object)){
 				RedisClusterCacheProvider.getResource().expire(cacheKeyBytes, 60);
 				object = null;
 			}
